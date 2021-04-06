cc3e5608541d8dfcbb62913ccaac01d4fa13186d
Per Rawdin <pr222iw@student.lnu.se>
2021-04-06 19:06:53 +0200

    Add ApolloServer in server.js

--- src/server.js
+++ src/server.js
@@ -10,3 +10,19 @@ const gateway = new ApolloGateway({
     { name: 'accounts', url: process.env.RESOURCE_DOMAIN }
   ]
 })
+
+// Pass the ApolloGateway to the ApolloServer constructor
+const server = new ApolloServer({
+  gateway,
+  // Disable subscriptions (not currently supported with ApolloGateway)
+  subscriptions: false,
+  /**
+   * Access req and res in context.
+   *
+   * @param {object} args server app.
+   * @param {object} args.req The request.
+   * @param {object} args.res The res.
+   * @returns {object} The req and res.
+   */
+  context: ({ req, res }) => ({ req, res })
+})
